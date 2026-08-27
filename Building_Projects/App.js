086ba6f7d47528e5fe1acf8c9389b77c89b30b48
Aniket Vyavahare <aniketvyavahare89301@gmail.com>
2026-08-27 21:31:40 +0530

    first header component

--- Building_Projects/App.js
+++ Building_Projects/App.js
@@ -3,13 +3,34 @@ import ReactDOM from "react-dom/client";
 
 const HeaderComp = () => {
   return (
-    <div className="header">
-      <div></div>
+    <div className="Header-Component">
+      <div className="Logo-Container">
+        <img
+          className="logo"
+          src="https://1000logos.net/wp-content/uploads/2021/05/Swiggy-logo.png"
+        />
+      </div>
+      <div className="Nav_items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact US</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
   );
 };
 const AppLayout = () => {
-  return <div>// header // body // footer</div>;
+  return (
+    <div>
+      <HeaderComp />
+      <div>Body</div>
+      <div>Footer</div>
+    </div>
+  );
 };
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+
+root.render(<AppLayout />);
